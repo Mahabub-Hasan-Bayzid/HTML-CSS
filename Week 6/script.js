0e4c19900cf641b20cfa33571d4bc9cc81abe3cc
Mahabub-Hasan-Bayzid <s2500143@edu.bc.fi>
2025-03-07 14:52:26 +0200

    HTML/CSS

---
+++ Week 6/script.js
@@ -0,0 +1,41 @@
+let backBtn= document.getElementById('backBtn');
+const overlay = document.querySelector('.overlay');
+const modalButton= document.querySelector('#seeBtn')
+const closeButton= document.querySelector('.modal button')
+
+
+
+
+const displayElement= ()=>{
+    overlay.classList.toggle('hidden');
+}
+
+const closeElement=()=>{
+    overlay.classList.toggle('hidden')
+}
+
+
+
+
+
+
+window.onscroll= function () {
+    scrollFunction();
+};
+const scrollFunction= ()=>{
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200){
+        backBtn.style.display= "block"
+    }
+    else{
+        backBtn.style.display= "none"
+    }
+}
+const backButton= ()=>{
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop= 0;
+    
+}
+
+backBtn.addEventListener('click',backButton);
+modalButton.addEventListener('click', displayElement);
+closeButton.addEventListener('click', closeElement);
